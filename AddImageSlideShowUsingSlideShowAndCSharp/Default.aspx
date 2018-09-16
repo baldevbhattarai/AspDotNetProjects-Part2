21137d6154af70d393c22329da9975ec0b70c22d
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-09-16 02:09:58 -0500

    Add project files.

---
+++ AddImageSlideShowUsingSlideShowAndCSharp/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AddImageSlideShowUsingSlideShowAndCSharp._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <br />
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <asp:Timer ID="Timer1" runat="server" Interval="1000" OnTick="Timer1_Tick"></asp:Timer>
+            <br />
+            <asp:Image ID="Image1" Hight="400px" Width="400px" runat="server" />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Stop SlideShow" OnClick="Button1_Click" />
+        </ContentTemplate>
+
+    </asp:UpdatePanel>
+
+
+</asp:Content>
---
+++ AddImageSlideShowUsingSlideShowAndCSharp/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AddImageSlideShowUsingSlideShowAndCSharp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                SetImageUrl();
+            }
+        }
+
+        private void SetImageUrl()
+        {
+            if (ViewState["ImageDisplayed"] == null)
+            {
+                Image1.ImageUrl = "~/Images/1.jpg";
+                ViewState["ImageDisplayed"] = 1;
+                Label1.Text = "Displaying Image - 1";
+            }
+            else
+            {
+                int i = (int)ViewState["ImageDisplayed"];
+                if (i == 8)
+                {
+                    Image1.ImageUrl = "~/Images/1.jpg";
+                    ViewState["ImageDisplayed"] = 1;
+                    Label1.Text = "Displaying Image - 1";
+                }
+                else
+                {
+                    i = i + 1;
+                    Image1.ImageUrl = "~/Images/" + i.ToString() + ".jpg";
+                    ViewState["ImageDisplayed"] = i;
+                    Label1.Text = "Displaying Image - " + i.ToString();
+                }
+            }
+        }
+
+        // This event is raised every one second as we have set
+        // the interval to 1000 milliseconds
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            SetImageUrl();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Timer1.Enabled)
+            {
+                Timer1.Enabled = false;
+                Button1.Text = "Start SlideShow";
+            }
+            else
+            {
+                Timer1.Enabled = true;
+                Button1.Text = "Stop SlideShow";
+
+            }
+        }
+    }
+}
