2ab1d8e839a28a63e4758b37b9759165122077e5
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-09-19 22:23:13 -0500

    Master page has been selected dynamically.

---
+++ AssigningAMasterPageDynamically/Site.Master.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AssigningAMasterPageDynamically
+{
+    public partial class Site : BaseMaterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public override Panel SearchPanel
+        {
+            get
+            {
+                return panelSearch;
+            }
+        }
+
+        public override string SearchTerm
+        {
+            get
+            {
+                return txtSerach.Text;
+            }
+        }
+
+        public override Button SearchButton
+        {
+            get
+            {
+                return btnSearch;
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged
+            (object sender, EventArgs e)
+        {
+            Session["SELECTED_MASTERPAGE"] = DropDownList1.SelectedValue;
+            Response.Redirect(Request.Url.AbsoluteUri);
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            if (Session["SELECTED_MASTERPAGE"] != null)
+            {
+                DropDownList1.SelectedValue =
+                    Session["SELECTED_MASTERPAGE"].ToString();
+            }
+        }
+    }
+}
---
+++ AssigningAMasterPageDynamically/WebForm1.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="AssigningAMasterPageDynamically.WebForm1" %>
+<%@ MasterType TypeName="AssigningAMasterPageDynamically.BaseMaterPage" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+</asp:Content>
